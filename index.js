c5db36b80261b43e84ffe1ec5922a410c7e1d0e3
Dinka228 <dan3vv@gmail.com>
2021-10-05 02:27:00 +0300

    fgf

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let mass1 = []
 let mass2 = []
 let mass3 = []
+let mass4 = []
 let body = []
+let bool = true
 let aNN = 1, minusANN, minusANN2,minusANN3,delta = 1
 let submitButton1 = document.getElementById('submitButton1')
 let submitButton2 = document.getElementById('submitButton2')
 let submitButton3 = document.getElementById('submitButton3')
+let submitButton4 = document.getElementById('submitButton4')
 let slar = document.getElementById('slar')
 slar.addEventListener('click',showForm1)
 let visitor = document.getElementById('visitor')
 visitor.addEventListener('click',showForm2)
 let wrapp = document.getElementById('wrapp')
 wrapp.addEventListener('click',showForm3)
+let simple = document.getElementById('simple')
+simple.addEventListener('click',showForm4)
 let window1 = document.getElementById('window1')
 let window2 = document.getElementById('window2')
 let window3 = document.getElementById('window3')
+let window4 = document.getElementById('window4')
 let answer = document.getElementById('answer')
 let form4 = document.getElementById('form4')
 function showForm1(){
@@ -26,15 +32,24 @@ function showForm2(){
     window1.style.display = 'none'
     window2.style.display = 'block'
     window3.style.display = 'none'
+    window4.style.display = 'none'
 }
 function showForm3(){
     window1.style.display = 'none'
     window2.style.display = 'none'
     window3.style.display = 'block'
+    window4.style.display = 'none'
+}
+function showForm4(){
+    window1.style.display = 'none'
+    window2.style.display = 'none'
+    window3.style.display = 'none'
+    window4.style.display = 'block'
 }
 submitButton1.addEventListener('click',mathSlar)
 submitButton2.addEventListener('click',mathVisitor)
 submitButton3.addEventListener('click',mathWrapp)
+submitButton4.addEventListener('click',mathSimple)
 function mathSlar(){
     let n = 5, m=6
 
@@ -304,4 +319,96 @@ function mathWrapp(){
             a.value = mass3[i][j]
         }
     }
+}
+function mathSimple(){
+    let n = 5, m=7
+
+    for(let i = 1;i<n;i++){
+        mass4[i] = [];
+        for(let j = 1;j<m;j++){
+            let b = ""
+            b=String(b)
+            i = String(i)
+            j = String(j)
+            b=i+j
+            let a = document.getElementById(`d${b}`)
+            mass4[i][j] = a.value;
+        }
+    }
+    console.log(mass4)
+    let min = 0, k = 0 , h = 0,min2 = 0,count = 1,k1 = 0,h1 = 0,k2 = 0,h2 = 0,max1 = 0,max2 = 0
+
+    for(let i = 1;i<n;i++){
+        min = +mass4[n-1][1]
+        for(let j = 1;j<m;j++){
+            if(+mass4[n-1][j]<= +min){
+                console.log(`${mass4[n-1][j]}<${min}`)
+                min = mass4[n-1][j]
+                h = j
+                if(count===1){
+                    h1 = h
+                }
+                else{
+                    h2 = h
+                }
+                console.log("СТАЛБЕЦ",h)
+            }
+        }
+        min2 = +mass4[i][m-1]/mass4[i][h]
+        if(min2<0){
+            min2 = mass4[i+1][m-1]/mass4[i+1][h]
+        }
+        for(let i = 1;i<n-1;i++){
+            console.log(`Вираз №${i}  ${mass4[i][m-1]}/${mass4[i][h]}`)
+            if(+mass4[i][m-1]/mass4[i][h] <= +min2 && +mass4[i][m-1]/mass4[i][h] > 0){
+                min2 = mass4[i][m-1]/mass4[i][h]
+                k = i
+                if(count===1){
+                    k1 = k
+                }
+                else{
+                    k2 = k
+                }
+            }
+        }
+        console.log(`Разрешающий элемент = ${mass4[k][h]} по адресу [${k}][${h}]`)
+            if(mass4[k][h] !== 1){
+                minusANN = mass4[k][h]
+                for(let j = 1;j<m;j++){
+                    mass4[k][j] = +mass4[k][j] / minusANN
+
+                }
+            }
+        for(let x = 1;x<n;x++){
+            if(x===k){
+                x++
+            }
+            minusANN = mass4[x][h]
+            for(let j = 1;j<m;j++){
+
+                mass4[x][j] = +mass4[x][j] - +mass4[k][j]*minusANN
+            }
+        }
+
+        for(let j = 1;j<m-1;j++){
+            if(mass4[n-1][j] < 0){
+                bool=false
+                break
+            }
+            else{
+                bool = true
+            }
+        }
+        if(bool === true){
+            break
+        }
+        count++
+    }
+    if(count<2){
+        answer.innerHTML = `x1 = ${mass4[k1][m-1]} x2 = 0 Fx = ${mass4[4][m-1]}`
+    }
+    if(count === 2){
+        answer.innerHTML = `x1 = ${mass4[k1][m-1]} x2 = ${mass4[k2][m-1]} Fx = ${mass4[4][m-1]}`
+    }
+
 }
